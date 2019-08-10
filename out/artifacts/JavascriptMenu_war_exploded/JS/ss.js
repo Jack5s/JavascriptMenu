634b374d26bedd313a9634ba361c44c08b6202d0
Jack5s <lijingwei1207@gmail.com>
2019-08-10 21:57:44 +0800

    Delete ss.js

--- out/artifacts/JavascriptMenu_war_exploded/JS/ss.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    document
-        .write("<script src='https://raw.githack.com/Jack5s/JavascriptMenu/master/web/JS/jquery.js'></script>");
-
-    document
-        .write("<script src='https://raw.githack.com/Jack5s/JavascriptMenu/master/web/JS/mymenu.js'></script>");
-
-})
